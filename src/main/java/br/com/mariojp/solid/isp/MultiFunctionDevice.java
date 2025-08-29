9ca3074e8831bfefa0de54f700841898199f0ab1
Lucas Benicio Santos <lucas.benicio@fbest.org.br>
2025-08-29 12:20:53 -0300

    aula 05

--- src/main/java/br/com/mariojp/solid/isp/MultiFunctionDevice.java
+++ src/main/java/br/com/mariojp/solid/isp/MultiFunctionDevice.java
@@ -1,7 +1,4 @@
 package br.com.mariojp.solid.isp;
 
-public interface MultiFunctionDevice {
-	void print(String content);
-
-	void scan(String targetFile);
+public interface MultiFunctionDevice extends Printer, Scanner {
 }
